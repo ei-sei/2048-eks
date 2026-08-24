0090d16a7f2e6f14d23f2593e85cf2feb4f42150
ei-sei <sk.ahm2019@gmail.com>
2026-08-24 16:15:01 +0100

    feat: add vpc module

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+environment        = "dev"
+availability_zones = ["eu-west-2a", "eu-west-2b"]
+vpc_cidr           = "10.0.0.0/16"
+name_prefix        = "2048"
---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to deploy resources in."
+  type        = list(string)
+  default     = ["eu-west-2a", "eu-west-2b"]
+}
+
+variable "environment" {
+  description = "The environment to deploy resources in (e.g., dev, staging, prod)."
+  type        = string
+  default     = "dev"
+}
+
+variable "name_prefix" {
+  description = "Prefix for resource names."
+  type        = string
+}
